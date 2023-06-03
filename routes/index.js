fc8520ca687fa8f1a7f3e06aa55ccdfd41a4ab12
Jason Flair <emelieobumse100@gmail.com>
2023-06-03 12:56:40 +0100

    task 7

--- routes/index.js
+++ routes/index.js
@@ -16,6 +16,6 @@ router.post('/files', FilesController.postUpload);
 router.get('/files/:id', FilesController.getShow);
 router.get('/files', FilesController.getIndex);
 router.put('/files/:id/publish', FilesController.putPublish);
-router.put('/files/:id/publish', FilesController.putUnpublish);
+router.put('/files/:id/unpublish', FilesController.putUnpublish);
 
 module.exports = router;
